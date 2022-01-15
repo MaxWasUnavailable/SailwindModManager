3e6d66b240d6cbc5371276711429a6c9f710d3f3
MaxWasUnavailable <89798523+MaxWasUnavailable@users.noreply.github.com>
2022-01-15 01:21:15 +0100

    Add fields to Mod.py

--- src/Mod/Mod.py
+++ src/Mod/Mod.py
@@ -6,18 +6,24 @@ class Mod:
     """
     Represents a mod.
     """
-    name: str
+    id: str
+    display_name: str
     description: str = None
     author: str = None
     version: str = None
+    manager_version: str = None
     game_version: str = None
+    requirements: list = field(default_factory=list)
+    incompatible: list = field(default_factory=list)
     tags: list = field(default_factory=list)
-    dependencies: list = field(default_factory=list)
-    url: str = None
+    download_url: str = None
+    home_page: str = None
+    repository: str = None
     image: bytes = None
+    modloader_required: bool = False
     additional_fields: dict = field(default_factory=dict)
 
 
 if __name__ == '__main__':
-    mod = Mod("Testmod", description="This is a test mod object.", author="Max", version="1.0.3", game_version="1.2.13b", tags=["Test", "Multiplayer", "Items"], dependencies=["TestModCore"], url="localhost")
+    mod = Mod("Testmod", "Testmod", description="This is a test mod object.", author="Max", version="1.0.3", game_version="1.2.13b", tags=["Test", "Multiplayer", "Items"], requirements=["TestModCore"], download_url="localhost")
     print(mod)
