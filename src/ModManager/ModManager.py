365ae40af8f6aff08cf996a7c1b247c28feadd8d
MaxWasUnavailable <89798523+MaxWasUnavailable@users.noreply.github.com>
2022-01-20 06:00:23 +0100

    Add ModManager support for installed mods and their management

--- src/ModManager/ModManager.py
+++ src/ModManager/ModManager.py
@@ -1,10 +1,12 @@
 from src.Logger.Loggable import Loggable
 from src.Config.Config import Config
 from src.Mod.Mod import Mod
+from shutil import rmtree, copytree
 from github import Github
 from copy import copy
 import requests
 import json
+import os
 
 
 class ModManager(Loggable):
@@ -19,11 +21,17 @@ class ModManager(Loggable):
     def __init__(self, logger, config: Config = None):
         super(ModManager, self).__init__(logger=logger)
         self.mods: dict[str, Mod] = dict()
+        self.installed_mods: dict[str, Mod] = dict()
         self.config: Config = config or Config()
         self.git: Github = self.__init_git()
 
         self.filter_tags = []
         self.filter_search = ""
+
+        # TODO: Might freeze the GUI for a couple of seconds if you have a lot of mods installed?
+        # Issue is, where to refresh these while keeping the fetching & refreshing split up between downloaded & installed mods on the GUI threads?
+        self.__refresh_downloaded_mods()
+        self.__refresh_installed_mods()
 
     def __init_git(self, use_token: bool = True) -> Github:
         """
@@ -39,12 +47,14 @@ class ModManager(Loggable):
         else:
             return Github()
 
-    def parse_mod(self, mod_data: dict, download_url: str = None, image: bytes = None) -> Mod:
+    def parse_mod(self, mod_data: dict, download_url: str = None, image: bytes = None, download_dir: str = None, install_dir: str = None) -> Mod:
         """
         Parse a mod's data and return a Mod object.
         :param mod_data: Mod's data as dictionary
         :param download_url: URL where the mod can be downloaded from
         :param image: Bytes object representing the mod's image
+        :param download_dir: Provided if the mod has already been downloaded
+        :param install_dir: Provided if the mod has already been installed
         :return: Parsed Mod object
         """
         self.log(f"Parsing new mod from {download_url}", is_verbose=True)
@@ -81,16 +91,60 @@ class ModManager(Loggable):
             repository=repository,
             image=image,
             modloader_required=modloader_required,
-            additional_fields=additional_fields
+            additional_fields=additional_fields,
+            downloaded_dir_path=download_dir,
+            installed_dir_path=install_dir
         )
 
-    def update_mod_list(self, mods: list[Mod]) -> None:
+    def update_mod_list(self, mods: list[Mod], installed: bool = False) -> None:
         """
+        # TODO: Might need a force_update parameter where downloaded/installed mod refreshes can be forced to replace existing entries? Might not be necessary though.
         Updates internal mod dictionary with new mod list.
         :param mods: list of mods to load from.
+        :param installed: Whether or not to update the installed mod list or downloaded mod list.
         """
         for mod in mods:
-            self.mods[mod.id] = mod
+            overwrite = False
+            if installed:
+                existing_entry = self.installed_mods.get(mod.id, None)
+            else:
+                existing_entry = self.mods.get(mod.id, None)
+            if existing_entry is None:
+                overwrite = True
+            else:
+                if existing_entry.downloaded_dir_path is not None:
+                    # Existing mod entry is downloaded
+                    if mod.downloaded_dir_path is None:
+                        # New mod is not downloaded
+                        # --> Update download URL in case of local copy not having one, or download URL having updated
+                        if installed:
+                            self.installed_mods[mod.id].download_url = mod.download_url
+                        else:
+                            self.mods[mod.id].download_url = mod.download_url
+
+                        if not existing_entry.compare_version(mod):
+                            # New mod object is newer in version
+                            if installed:
+                                self.installed_mods[mod.id].update_available = True
+                            else:
+                                self.mods[mod.id].update_available = True
+                    else:
+                        # If *new* entry is downloaded, we (probably always?) have just updated a mod. Hence, overwrite.
+                        overwrite = True
+                else:
+                    # If both aren't downloaded, we received new information. Hence, overwrite.
+                    overwrite = True
+            if overwrite:
+                if installed:
+                    self.installed_mods[mod.id] = mod
+                    if self.installed_mods[mod.id].download_url is None and existing_entry is not None:
+                        # If old entry had a download URL and new one doesn't, update new entry with old download URL.
+                        self.installed_mods[mod.id].download_url = existing_entry.download_url
+                else:
+                    self.mods[mod.id] = mod
+                    if self.mods[mod.id].download_url is None and existing_entry is not None:
+                        # If old entry had a download URL and new one doesn't, update new entry with old download URL.
+                        self.mods[mod.id].download_url = existing_entry.download_url
 
     def fetch_info(self, second_attempt: bool = False) -> bool:
         """
@@ -152,6 +206,7 @@ class ModManager(Loggable):
         downloads_dir = self.config.config.get("downloads_directory", "")
         if mod.download(downloads_dir):
             self.log("Mod downloaded successfully.")
+            self.__refresh_downloaded_mods()
             return True
         else:
             self.log("Mod download failed.")
@@ -170,6 +225,8 @@ class ModManager(Loggable):
         """
         if clear:
             self.clear_mods()
+        self.__refresh_downloaded_mods()
+        self.__refresh_installed_mods()
         self.fetch_info()
 
     def add_filter_tag(self, tag: str) -> None:
@@ -249,6 +306,66 @@ class ModManager(Loggable):
         """
         return sorted(mods, key=lambda mod: mod.display_name)
 
+    def refresh_local_mods(self, directory: str, downloaded: bool = False, installed: bool = False) -> list[Mod]:
+        mods = []
+        if directory is None:
+            self.log("Tried to refresh from local mods directory without a directory being given.", is_error=True)
+            return mods
+
+        if not os.path.exists(directory):
+            self.log("Tried to refresh from local mods directory with invalid directory specified. This could be because you haven't downloaded any mods yet, or because you don't have a mods folder in the Sailwind directory.", is_error=True)
+            return mods
+
+        for local_mod_dir in [os.path.join(directory, mod_dir_name) for mod_dir_name in filter(lambda x: os.path.isdir(os.path.join(directory, x)), os.listdir(directory))]:
+            try:
+                mod_image = None
+                data = None
+                self.log(f"Fetching local mod from: \"{local_mod_dir}\"")
+                for file in os.listdir(local_mod_dir):
+                    if file == "info.json":
+                        info_file = open(os.path.join(local_mod_dir, file), 'r')
+                        data = json.loads(info_file.read())
+                        info_file.close()
+                    if file in ["mod.png", "mod.jpg"]:
+                        image_file = open(os.path.join(local_mod_dir, file), 'r', encoding="raw_unicode_escape")
+                        mod_image = image_file
+                        image_file.close()
+
+                if data is not None:
+                    download_dir = None
+                    install_dir = None
+                    if downloaded:
+                        download_dir = local_mod_dir
+                    if installed:
+                        install_dir = local_mod_dir
+                    mods.append(self.parse_mod(data, image=mod_image, download_dir=download_dir, install_dir=install_dir))
+                    self.log(f"Parsed new mod: \"{mods[-1].display_name}\"")
+                else:
+                    self.log(f"Mod found without info.json file. Please report this to the modding community! Mod in question is: \"{local_mod_dir}\"")
+            except Exception as e:
+                self.log(str(e), is_error=True)
+
+        self.update_mod_list(mods, installed=installed)
+
+    def __refresh_downloaded_mods(self):
+        downloaded_dir = self.config.config.get("downloads_directory", None)
+        self.refresh_local_mods(downloaded_dir, downloaded=True)
+
+    def __refresh_installed_mods(self):
+        installed_dir = self.config.config.get("game_directory", None)
+        if installed_dir is not None:
+            installed_dir += "/mods"
+        self.installed_mods.clear()
+        self.refresh_local_mods(installed_dir, installed=True)
+
+    def get_mod(self, mod_id: str) -> Mod:
+        """
+        Get a specific mod object if it exists.
+        :param mod_id: Mod id of the mod to get
+        :return:
+        """
+        return self.mods.get(mod_id, None)
+
     def get_mods(self) -> list[Mod]:
         """
         Get list of fetched & filtered mods. Does not re-fetch from repos.
@@ -261,14 +378,103 @@ class ModManager(Loggable):
 
         return self.sort_mods(mods_filtered_search)
 
-    def get_installed_mods(self) -> list[Mod]:
-        return self.get_mods()
+    def get_installed_mods(self, refresh: bool = True) -> list[Mod]:
+        """
+        Get list of installed mods.
+        :param refresh: Whether or not to check the mods directory again for changes.
+        :return: List of installed mods.
+        """
+        if refresh:
+            self.__refresh_installed_mods()
 
-    def install_mod(self, mod_id: str) -> None:
-        pass
+        mods = list(self.installed_mods.values())
 
-    def uninstall_mod(self, mod_id: str) -> None:
-        pass
+        installed_mods = []
+        for mod in mods:
+            if mod.installed_dir_path is not None:
+                installed_mods.append(mod)
 
-    def update_mod(self, mod_id: str) -> None:
-        pass
+        return installed_mods
+
+    def install_mod(self, mod_id: str) -> bool:
+        """
+        Copy a mod from the downloads dir to the installation dir.
+        :param mod_id: Mod id of the mod to install
+        :return: Whether or not the installation was successful.
+        """
+        installed_dir = self.config.config.get("game_directory", None)
+
+        if installed_dir is None:
+            self.log("Game installation directory was not specified. Please check your configuration file.")
+            return False
+
+        mod_to_install = self.mods.get(mod_id, None)
+        if mod_to_install is None:
+            self.log("Could not find mod to install since it wasn't found in the mod list.")
+            return False
+
+        mod_downloaded_dir = mod_to_install.downloaded_dir_path
+        if mod_downloaded_dir is None:
+            self.log("Could not install mod since it wasn't downloaded.")
+            return False
+
+        if not os.path.exists(mod_downloaded_dir):
+            self.log("Could not install mod since it wasn't downloaded. Please report this.")
+            return False
+
+        if mod_to_install.installed_dir_path is not None:
+            if os.path.exists(mod_to_install.installed_dir_path):
+                self.log("Mod is already installed. Please ensure that there are no leftover files of this mod in the mods directory. Please report this.")
+                return False
+
+        folder_name = mod_downloaded_dir.split("/")[-1]
+
+        if os.path.exists(f"{installed_dir}/mods/{folder_name}"):
+            self.log("Mod is already installed. Please ensure that there are no leftover files of this mod in the mods directory.")
+            return False
+
+        copytree(mod_downloaded_dir, f"{installed_dir}/mods/{folder_name}")
+
+        self.log(f"Mod installed in directory: {installed_dir}/mods/{folder_name}")
+
+        self.__refresh_installed_mods()
+
+        return True
+
+    def uninstall_mod(self, mod_id: str) -> bool:
+        """
+        Remove a mod from the installation dir.
+        :param mod_id: Mod id of the mod to uninstall
+        :return: Whether or not the uninstallation was successful.
+        """
+        installed_mod = self.installed_mods.get(mod_id, None)
+        if installed_mod is None:
+            self.log("Could not uninstall mod since it wasn't found in the installed mod list.")
+            return False
+        if os.path.exists(installed_mod.installed_dir_path):
+            self.log(f"Uninstalling mod from {installed_mod.installed_dir_path}")
+            rmtree(installed_mod.installed_dir_path)
+            self.__refresh_installed_mods()
+            return True
+        else:
+            self.log("Could not uninstall mod since the installed path wasn't found. Please report this.")
+            return False
+
+    def update_mod(self, mod_id: str, update_install: bool = True, download_first: bool = False) -> None:
+        """
+        Update a mod.
+        :param mod_id: Mod id of the mod to uninstall
+        :param update_install: Whether or not to update the installed mod as well
+        :param download_first: Whether or not to force a new download first before updating the installed mod. Generally only useful if you're updating both the download *and* the installed mod at the same time.
+        :return: Whether or not the update was successful.
+        """
+        if update_install:
+            success = True
+            if download_first:
+                success = self.download_mod(mod_id)
+            success = success and self.install_mod(mod_id)
+
+        else:
+            success = self.download_mod(mod_id)
+
+        return success
