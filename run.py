567863ba530758acf04ce76fc5da9b14b80adcc8
MaxWasUnavailable <89798523+MaxWasUnavailable@users.noreply.github.com>
2022-01-20 04:20:00 +0100

    Remove test function from run.py

--- run.py
+++ run.py
@@ -6,33 +6,6 @@ from PySide2.QtWidgets import QApplication
 import sys
 
 
-def test_basic():
-    logger = Logger(logfile_name="test.log")
-    manager = ModManager(logger=logger)
-    manager.refresh()
-    # manager.download_mod("TestMod")
-
-    m1 = manager.get_mods()
-
-    manager.add_filter_tag("Test")
-    m2 = manager.get_mods()
-
-    manager.set_filter_search("Test")
-    m3 = manager.get_mods()
-
-    manager.remove_filter_tag("Test")
-    m4 = manager.get_mods()
-
-    manager.add_filter_tag("Nothing")
-    m5 = manager.get_mods()
-
-    print([mod.display_name for mod in m1])
-    print([mod.display_name for mod in m2])
-    print([mod.display_name for mod in m3])
-    print([mod.display_name for mod in m4])
-    print([mod.display_name for mod in m5])
-
-
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
